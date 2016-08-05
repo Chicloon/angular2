e27ce3bed5541d73a620dfd4ae910522680239c0
Chicloon <Chicloon@gmail.com>
2016-08-05 11:15:24 +0900

    Простое  todo

--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,24 @@
-import { bootstrap }    from '@angular/platform-browser-dynamic';
-import { AppComponent } from './app.component';
-bootstrap(AppComponent);
+import { Component } from '@angular/core';
+import { bootstrap } from '@angular/platform-browser-dynamic';
+
+@Component({
+    selector: 'todo-app',
+    template: `<h1> {{ title }} </h1>
+        <ul> 
+            <li *ngFor="#todo of todos">{{ todo }} </li>
+            
+        </ul>
+    `
+})
+
+class AppComponent {
+    title: string;
+    todos: string[];
+
+    constructor() {
+        this.title = 'Todo';
+        this.todos = [' Иузчить Ангуяр', 'Иузчить TypeScript', 'Изучить Node.js'];
+    }
+}
+
+bootstrap (AppComponent);
