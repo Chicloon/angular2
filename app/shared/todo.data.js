16c7527d2d7eb97a05a888c99c620887d613f7b3
Efim Khlebnyy <Chicloon@gmail.com>
2016-08-15 16:13:08 +0900

    cleanup

--- app/shared/todo.data.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.todos = [
-    { title: 'Изучить TypeScript', done: false },
-    { title: 'Изучить ES6', done: false },
-    { title: 'Изучить Angular2', done: false },
-    { title: 'Отдохнуть', done: false },
-];
-//# sourceMappingURL=todo.data.js.map
